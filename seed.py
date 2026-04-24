272e6dce9bc091810d0cae6b4581fe0161834c1b
Blessed Adesina <adesinablessed19@gmail.com>
2026-04-24 02:17:06 +0100

    code added

--- seed.py
+++ seed.py
@@ -3,22 +3,31 @@ import gdown
 from database import Profile, get_db
 
 def download_and_seed():
-    url = "https://drive.google.com/uc?id=1Up06dcS9OfUEnDj_u6OV_xTRntupFhPH"
+    url = "https://drive.google.com/uc?export=download&id=1Up06dcS9OfUEnDj_u6OV_xTRntupFhPH"
     gdown.download(url, "seed_profiles.json", quiet=False, fuzzy=True)
     
     with open("seed_profiles.json", "r") as f:
-        profiles = json.load(f)
+        raw_data = json.load(f)
     
+    profiles =[]
+    if isinstance(raw_data, dict):
+        for value in raw_data.values():
+            if isinstance(value, list):
+                profiles = value
+                break
+    else:
+        profiles = raw_data
+
     db = next(get_db())
     added = 0
     
     for p in profiles:
-        existing = db.query(Profile).filter(Profile.name == p .lower()).first()
+        existing = db.query(Profile).filter(Profile.name == p.get("name", "").lower()).first()
         if existing:
             continue
             
         profile = Profile(
-            name=p .lower(),
+            name=p.get("name", "").lower(),
             gender=p.get("gender"),
             gender_probability=p.get("gender_probability"),
             age=p.get("age"),
